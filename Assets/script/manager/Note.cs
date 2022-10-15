d0b9f69763f2b6dc02e318c26f55400fcc61be2f
YeongJUnJoH171 <44230805+YeongJUnJoH171@users.noreply.github.com>
2022-10-15 18:56:29 +0900

    노트 생성 및 움직임 추가
    노트 생성 움직임 추가

---
+++ Assets/script/manager/Note.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+
+    public float noteSpeed = 400;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += Vector3.down * noteSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/script/manager/NoteManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public int bpm = 0;
+    double currentTime = 0d;
+
+    [SerializeField] Transform tfNoteAppear = null;
+    [SerializeField] GameObject goNote = null;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime; 
+
+        if (currentTime >= 60d / bpm)
+        {
+            GameObject t_Note = Instantiate(goNote, tfNoteAppear.position, Quaternion.identity);
+            t_Note.transform.SetParent(this.transform);
+            currentTime -= 60d / bpm;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Note"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
