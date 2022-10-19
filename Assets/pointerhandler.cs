8405b7abb3d21b9acbd16cfeac49dc2fbd733532
Yeon-junLee <yunjoon123@naver.com>
2022-10-19 17:04:29 +0900

    button click시 함수 실행

--- Assets/pointerhandler.cs
+++ Assets/pointerhandler.cs
@@ -6,6 +6,7 @@ using Valve.VR.Extras;
 public class pointerhandler : MonoBehaviour
 {
     public SteamVR_LaserPointer laserPointer;
+    private onClick_UI onclick = null;
 
     // Start is called before the first frame update
     void Start()
@@ -13,6 +14,8 @@ public class pointerhandler : MonoBehaviour
         laserPointer.PointerIn += PointerInside;
         laserPointer.PointerOut += PointerOutside;
         laserPointer.PointerClick += PointerClick;
+
+        onClick_UI onclick = GetComponent<onClick_UI>();
     }
 
     public void PointerInside(object sender, PointerEventArgs eventArgs)
@@ -27,9 +30,24 @@ public class pointerhandler : MonoBehaviour
 
     public void PointerClick(object sender, PointerEventArgs eventArgs)
     {
-        if (eventArgs.target.CompareTag("UI"))
+        if (eventArgs.target.CompareTag("EXIT"))
         {
-            Debug.Log(eventArgs.target.name);       // 클릭 버튼 생각해서 함수 또 짜야됨 지금 기기 없이는 확인 못함
+            onclick.ExitMenu_btn_clicked();       // 클릭 버튼 생각해서 함수 또 짜야됨 지금 기기 없이는 확인 못함
+        }
+
+        if (eventArgs.target.CompareTag("EXIT_BACK"))
+        {
+            onclick.ExitMenu_btn_back_clicked();
+        }
+
+        if (eventArgs.target.CompareTag("OPTION"))
+        {
+            onclick.OptionMenu_btn_clicked();
+        }
+
+        if (eventArgs.target.CompareTag("OPTION_BACK"))
+        {
+            onclick.OptionMenu_btn_back_clicked();
         }
 
     }
