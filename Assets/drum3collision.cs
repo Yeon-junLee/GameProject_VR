b0c5da78c145269f892d20b26e893a0f0316d5e5
Yeon-junLee <83509116+Yeon-junLee@users.noreply.github.com>
2022-10-30 18:17:45 +0900

    commit again

---
+++ Assets/drum3collision.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drum3collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // drum 소리 나오는 함수
+        // 게임에서 노트 파괴하는 함수
+    }
+}
